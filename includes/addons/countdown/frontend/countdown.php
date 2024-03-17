4f47fff7699bac49d8a54232f3ae50817aa98233
mdhemalakhand <hemalrika@gmail.com>
2024-03-18 02:48:43 +0600

    updated

--- includes/addons/countdown/frontend/countdown.php
+++ includes/addons/countdown/frontend/countdown.php
@@ -39,26 +39,6 @@ class Countdown {
         add_action( 'rws_show_product_countdown_single', array( $this, 'rws_product_countdown_single' ) );
     }
     /**
-     * Create a product single countdown
-     * 
-     * @since 1.0.0
-     * 
-     * @return void
-     */
-    public function rws_product_countdown_single( $end_date ) {
-        var_dump( $end_date );
-        ob_start();
-            echo <<<EOD
-            <div class="rws-timer" data-date="$end_date">
-                <div id="days"></div>
-                <div id="hours"></div>
-                <div id="minutes"></div>
-                <div id="seconds"></div>
-            </div>
-            EOD;
-        echo ob_get_clean();
-    }
-    /**
      * Create a function for show countdown in product details page
      * 
      * This functionality will be available when admin will set date from ( woocommerce is first priority and rws plugin is 2nd priority)
@@ -77,17 +57,23 @@ class Countdown {
         $product_id = get_the_ID();
         $product_object = array_map( 'sanitize_text_field', get_post_meta( $product_id, 'rws_product_objects' ));
         $product_object_arr = isset( $product_object[0] )  ? unserialize( $product_object[0] ): array();
-        $rws_product_single_countdown_from = '2024-03-18';
-        $rws_product_single_countdown_to   = '2024-05-19';
+        $schedule_date_start = get_post_meta($product_id, '_sale_price_dates_from', true);
+        $schedule_date_end = get_post_meta($product_id, '_sale_price_dates_to', true);
+        $date_end = date( 'Y-m-d', $schedule_date_end );
         /**
          * Create a cron for handle schedule So that, timer shows only when date from started.
          */
-        $arg = array(
-            'emal' => $rws_product_single_countdown_to
-        );
-        $timestamp = strtotime($rws_product_single_countdown_from);
-        if( !wp_next_scheduled( 'rws_show_product_countdown_single', $arg  ) ) {
-            wp_schedule_single_event( $timestamp, 'rws_show_product_countdown_single', $arg ); 
+        if( ( time() >= $schedule_date_start ) && ( time() <= $schedule_date_end ) ) {
+            ob_start();
+                echo <<<EOD
+                <div class="rws-timer" data-date="$date_end">
+                    <div id="days"></div>
+                    <div id="hours"></div>
+                    <div id="minutes"></div>
+                    <div id="seconds"></div>
+                </div>
+                EOD;
+            echo ob_get_clean();
         }
     }
 }
